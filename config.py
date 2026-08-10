882b44954ae71bb9838a699f2fac047a490bd5e0
gohyichern <gohyc-wm25@student.tarc.edu.my>
2026-08-10 09:07:58 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "gohyichern-employee"
+customregion = "us-east-1"
+
